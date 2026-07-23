ebd00ddce01ee26360c27e9ba9897e58b2580ad8
Idris Westbrook <westbrook.idris@gmail.com>
2026-07-23 16:12:17 -0400

    add type casting

--- lesson2.py
+++ lesson2.py
@@ -1,10 +1,27 @@
 
 #* Inputs
-x = input('Enter any number: ')
-print('You have entered', x)
-n = input('Enter your name: ')
-print('You have entered', x, 'Hello,', n,)
+#x = input('Enter any number: ')
+#print('You have entered', x)
+#n = input('Enter your name: ')
+#print('You have entered', x, 'Hello,', n)
 
 #! By default, the input() function treats all inputs as strings
 
-print(type(x))
+#print(type(x))
+
+#* Type casting can be used in any data type to manually convert it to another
+a = 45.77
+print(type(a))
+a = int(a)
+print(a)
+print(type(a))
+
+#* Convert a user input to another data type
+#y = input('Enter a number ')
+#y = int(y)
+#print(y - 5)
+#print(type(y))
+
+y = int(input('Enter first number '))
+z = int(input('Enter second number '))
+print(y + z)
