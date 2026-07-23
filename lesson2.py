5ff71072a26c3e6e111c8a68a69109dd1e33cac1
Idris Westbrook <iwestbrook@choate.edu>
2026-07-23 15:01:35 -0400

    add inputs

---
+++ lesson2.py
@@ -0,0 +1,10 @@
+
+#* Inputs
+x = input('Enter any number: ')
+print('You have entered', x)
+n = input('Enter your name: ')
+print('You have entered', x, 'Hello,', n,)
+
+#! By default, the input() function treats all inputs as strings
+
+print(type(x))
