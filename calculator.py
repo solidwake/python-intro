d1d21253182270a8d13450fed18edf3c673d91df
Idris Westbrook <westbrook.idris@gmail.com>
2026-07-28 09:56:48 -0400

    add calculator

---
+++ calculator.py
@@ -0,0 +1,8 @@
+a = int(input('Enter first number '))
+b = int(input('Enter second number '))
+
+print('Addition is: ', a + b)
+print('Multiplication is: ', a * b)
+print('Remainder is: ', a % b)
+print('Subtraction is: ', a - b)
+print('Divsion is: ', a / b)
