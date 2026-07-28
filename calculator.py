68db28b4cb69ef87783166bbda8975ad8337c66f
Idris Westbrook <westbrook.idris@gmail.com>
2026-07-28 10:23:13 -0400

    add stored variables for calculations

--- calculator.py
+++ calculator.py
@@ -1,8 +1,14 @@
 a = int(input('Enter first number '))
 b = int(input('Enter second number '))
 
-print('Addition is: ', a + b)
-print('Multiplication is: ', a * b)
-print('Remainder is: ', a % b)
-print('Subtraction is: ', a - b)
-print('Divsion is: ', a / b)
+sum = a + b
+product = a * b
+modulus = a % b
+difference = a - b
+quotient = a / b
+
+print('Addition is: ', sum)
+print('Multiplication is: ', product)
+print('Remainder is: ', modulus)
+print('Subtraction is: ', difference)
+print('Divsion is: ', quotient)
