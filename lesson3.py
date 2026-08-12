ad7757cda1e021825b3938a21d3960a44a5e45c8
Idris Westbrook <westbrook.idris@gmail.com>
2026-08-12 12:46:35 -0400

    add else if statment

--- lesson3.py
+++ lesson3.py
@@ -8,3 +8,18 @@ if a > b:
     print('True')
 else:
     print('False')
+
+#*Else if statements
+
+score = 93
+
+if score > 90:
+    print('You got an A')
+elif score > 80:
+    print('You got a B')
+elif score > 70:
+    print('You got a C')
+elif score > 60:
+    print('You got a D')
+else:
+    print('You failed.')
